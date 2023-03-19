ab07b7bd66402d97e40014463cd06badcd6466f6
jhojan <alejoyepes.18@gmail.com>
2023-03-19 08:50:17 -0500

    ajuste enums

---
+++ WebApiHiringItm.CORE/Helpers/Enums/File/FileEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApiHiringItm.CORE.Helpers.Enums.File
+{
+    public enum FileEnum
+    {
+        [Display(Description = "Planilla")]
+        PLANILLA = 0,
+        [Display(Description = "Informe De Ejecucion")]
+        INFORME = 1,
+        [Display(Description = "Cuenta De Cobro")]
+        CUENTADECOBRO = 2,
+        [Display(Description = "Contrato")]
+        CONTRATO = 3,
+    }
+}
---
+++ WebApiHiringItm.CORE/Helpers/Enums/Rolls/RollEnum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiHiringItm.CORE.Helpers.Enums.Rolls
+{
+    public enum RollEnum
+    {
+        [Display(Description = "ADM")]
+        Admin = 0,
+        [Display(Description = "CTL")]
+        Contractual = 1,
+        [Display(Description = "NMA")]
+        Nomina = 2,
+        [Display(Description = "PLNC")]
+        Planeacion = 3,
+        [Display(Description = "JURD")]
+        Juridico = 4,
+        [Display(Description = "CMT")]
+        Comite = 5,
+        [Display(Description = "SPV")]
+        Supervisor = 6,
+        [Display(Description = "CTA")]
+        Contratista = 7,
+        [Display(Description = "DTV")]
+        Desactivada = 8,
+
+    }
+}
