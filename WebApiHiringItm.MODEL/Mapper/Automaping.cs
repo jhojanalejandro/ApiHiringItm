489f5a68b0d233a5a9dad15c96bd10a8433206b0
jhojanhernandezy24 <jhojanhernandezy24@outlook.es>
2023-07-21 00:47:07 -0500

    ajuste modelo

--- WebApiHiringItm.MODEL/Mapper/Automaping.cs
+++ WebApiHiringItm.MODEL/Mapper/Automaping.cs
@@ -36,7 +36,22 @@ namespace WebApiHiringItm.MODEL.Mapper
             CreateMap<ElementComponentDto, ElementComponent>().ReverseMap();
             CreateMap<DetalleContratoDto, DetailContract>().ReverseMap();
             CreateMap<UserFileDto, UserFile>().ReverseMap();
-            CreateMap<RProjectForlderDto, ContractFolder>().ReverseMap();
+            CreateMap<RProjectForlderDto, ContractFolder>()
+                .ForMember(c => c.Id, cd => cd.MapFrom(src => src.Id))
+                .ForMember(c => c.UserId, cd => cd.MapFrom(src => Guid.Parse(src.UserId)))
+                .ForMember(c => c.Rubro, cd => cd.MapFrom(src => Guid.Parse(src.Rubro)))
+                .ForMember(c => c.StatusContractId, cd => cd.MapFrom(src => Guid.Parse(src.StatusContractId)))
+                .ForMember(c => c.ObjectContract, cd => cd.MapFrom(src => src.ObjectContract))
+                .ForMember(c => c.ContractorsCant, cd => cd.MapFrom(src => src.ContractorsCant))
+                .ForMember(c => c.Activate, cd => cd.MapFrom(src => src.Activate))
+                .ForMember(c => c.EnableProject, cd => cd.MapFrom(src => src.EnableProject))
+                .ForMember(c => c.CompanyName, cd => cd.MapFrom(src => src.CompanyName))
+                .ForMember(c => c.ProjectName, cd => cd.MapFrom(src => src.ProjectName))
+                .ForMember(c => c.Project, cd => cd.MapFrom(src => src.Project))
+                .ForMember(c => c.FuenteRubro, cd => cd.MapFrom(src => src.FuenteRubro))
+                .ForMember(c => c.NumberProject, cd => cd.MapFrom(src => src.NumberProject))
+
+                .ReverseMap();
             CreateMap<ProjectFolderCostsDto, ContractFolder>().ReverseMap();
             CreateMap<DetailFileDto, DetailFile>().ReverseMap();
             CreateMap<Activity, ActivityDto>().ReverseMap();
